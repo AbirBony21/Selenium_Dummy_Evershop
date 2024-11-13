94ebfed972b4962093aa49720b42417f3c3ddb47
AbirBony21 <abirbony@gmail.com>
2024-11-14 03:28:40 +0600

    update add to cart xpath

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function testRun() {
     await driver.sleep(2000);
     await driver.findElement(By.xpath("//input[@name='qty']")).sendKeys(2);
     await driver.sleep(2000);
-    await driver.findElement(By.xpath("//button[contains(.,'ADD TO CART')]")).click();
+    await driver.findElement(By.xpath("//button[contains(.,'CART')]")).click();
     await driver.sleep(2000);
     await driver.findElement(By.xpath("//a[@class='add-cart-popup-button']")).click();
     
